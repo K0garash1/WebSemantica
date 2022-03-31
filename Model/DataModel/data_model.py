cdc0fbeea7a57e9a4177fb5680b11515499dc37c
K0garash1 <62295488+K0garash1@users.noreply.github.com>
2022-03-31 06:01:23 -0500

    Update data_model.py

--- Model/DataModel/data_model.py
+++ Model/DataModel/data_model.py
@@ -1,3 +1,10 @@
 '''
 Modelo de datos
 '''
+#Genera una consulta sparql con los datos suministrados
+def gen_query(title, author, year, publisher, typeResource, language, keywords):
+  query = 'SELECT ?s WHERE {?x creator ?creator . FILTER(contains(ucase(?creator),'+author+'))}'
+  return query
+#Realiza una peticion al servidor Fuseki con la consulta correspondiente
+def query_data(query):
+  return requests.post('http://localhost:puerto/fuseki', data={'query': query})
