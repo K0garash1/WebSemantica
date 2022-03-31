f2b9ade96084b688e0d45255bd0f08358cd3e29b
K0garash1 <62295488+K0garash1@users.noreply.github.com>
2022-03-31 05:48:35 -0500

    Update user_controller.py

--- Controllers/UserController/user_controller.py
+++ Controllers/UserController/user_controller.py
@@ -1,7 +1,18 @@
+'''
+Controlador de usuarios
+Se encarga de administrar los datos de acceso para la sesion de un
+usuario tipo administrador
+'''
 import sys
 sys.path.insert(1, 'Model/UserModel')
 from user_model import query_user
 
+'''
+Modulo de inicio de sesion, recive un formulario con los campos 'email' y 'password'
+Busca al usuario por email en la base de datos y compara las contraseñas
+Si coinciden se establece una sesion activa
+Si no coinciden se elimina la sesion
+'''
 def user_login(session, form):
     email = form['email']
     password = form['password']
@@ -13,6 +24,10 @@ def user_login(session, form):
     else:
         session.pop('loggedin', None)
 
+'''
+Cierre de sesion
+Limpia los campos dela sesion actual y desactiva el estado 'loggedin'
+'''
 def user_logout(session):
     session.pop('loggedin', None)
     session.pop('email', None)
