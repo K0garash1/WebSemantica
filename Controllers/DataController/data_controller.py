a3f4cf093cc800f0ddd1a4895f4fe02e7c7c1cb9
K0garash1 <62295488+K0garash1@users.noreply.github.com>
2022-03-31 06:00:21 -0500

    Update data_controller.py

--- Controllers/DataController/data_controller.py
+++ Controllers/DataController/data_controller.py
@@ -1,12 +1,24 @@
+'''
+Controlador de datos
+Se encarga de administrar la consulta sparql a Fuseki y su respuesta
+'''
+import sys
+sys.path.insert(1, 'Model/DataModel')
+from data_model import gen_query, query_data
+
+'''
+Recibe los datos del formulario e identifica los campos, genera una consulta sparql mediante el modelo
+y hace una peticion al servidor fuseki
+'''
 def send_query(form):
     title = form['title']
     author = form['author']
     year = form['year']
     publisher = form['publisher']
-    type = form['type']
+    typeResource = form['type']
     language = form['language']
     keywords = form['keywords']
-    query = 'SELECT ?s WHERE {?x creator ?creator . FILTER(contains(ucase(?creator),'+author+'))}'
-    response = requests.post('http://localhost:puerto/fuseki',
-       data={'query': query})
+    
+    query = gen_query(title, author, year, publisher, typeResource, language, keywords)
+    response = query_data(query)
     return response.json()
